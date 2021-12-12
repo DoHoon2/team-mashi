6efef79baad6542401fb92aa63b2dc9cc3044c16
DoHoon2 <dohuen304@naver.com>
2021-12-12 17:09:10 +0900

    feat : random number _ test

---
+++ pig-dice.js
@@ -0,0 +1,7 @@
+let button = document.querySelector(".clickbtn");
+
+// 시작
+function number(){
+    const randomNum = Math.floor(Math.random() * 6 + 1);
+}
+button.addEventListener("click", number);
